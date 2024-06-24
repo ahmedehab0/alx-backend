25b619d225513069a97900a746ea3b23eac8bd0f
ahmedehab0 <ahmed.eh01@gmail.com>
2024-06-24 09:57:38 -0400

    script

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """module to create a helper function"""
 
 from typing import Tuple
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size
