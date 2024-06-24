a7ea0ac1bedf205cf7d92cd44f8b1bbb0787384b
ahmedehab0 <ahmed.eh01@gmail.com>
2024-06-24 09:58:58 -0400

    script

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """calculate the start and end of content in a page"""
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
